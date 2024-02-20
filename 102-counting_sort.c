a18933e7961bacbe765508ea4a01e769a4aebd52
Karushi536 <karushiabigael@gmail.com>
2024-02-20 23:27:01 +0300

    i tried fixing errors

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,44 +2,45 @@
 #include <stdlib.h>
 
 /**
- * counting_sort - sorts an array of integers in ascending order using Counting sort algorithm
+ * counting_sort - sorts an array of integers in
+ * ascending order using Counting sort algorithm
+ * print_array - prints an array of integers
  * @array: array of integers
  * @size: size of the array
  */
-void counting_sort(int *array, size_t size) {
-    if (size < 2)
-        return;
+void counting_sort(int *array, size_t size)
+{
+	int n, j, *count_array, *aux;
+	size_t i;
 
-    size_t i;
-    int max = array[0];
-    for (i = 1; i < size; i++) {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-    int *count = malloc((max + 1) * sizeof(int));
-    if (count == NULL)
-        return;
-
-    int j;
-    for (j = 0; j <= max; j++)
-        count[j] = 0;
-
-    for (i = 0; i < size; i++)
-        count[array[i]]++;
-
-    printf("%d", count[0]);
-    for (j = 1; j <= max; j++)
-        printf(", %d", count[j]);
-    printf("\n");
-
-    int index = 0;
-    for (j = 0; j <= max; j++) {
-        while (count[j] > 0) {
-            array[index++] = j;
-            count[j]--;
-        }
-    }
-
-    free(count);
+	if (!array || size < 2)
+		return;
+	n = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > n)
+			n = array[i];
+	}
+	count_array = calloc((n + 1), sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		count_array[array[i]]++;
+	}
+	for (j = 1; j < n; j++)
+	{
+		count_array[j + 1] += count_array[j];
+	}
+	print_array(count_array, n + 1);
+	aux = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		count_array[array[i]]--;
+		aux[count_array[array[i]]] = array[i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = aux[i];
+	}
+	free(aux);
+	free(count_array);
 }
